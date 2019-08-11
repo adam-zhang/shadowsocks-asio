bdd3e8d972faaa01f590e262ff202e65eb3cc41d
adam zhang <mingpingzhang@163.com>
2019-08-12 07:47:14 +0800

    the command line parse with is done.

--- source/common/JsonParser.cpp
+++ source/common/JsonParser.cpp
@@ -1,5 +1,11 @@
 #include "JsonParser.h"
 #include "json.hpp"
+#include "GlobalVariable.h"
+#include "Options.h"
+#include <fstream>
+
+using namespace std;
+using json = nlohmann::json;
 
 JsonParser::JsonParser()
 {
@@ -9,7 +15,23 @@ JsonParser::~JsonParser()
 {
 }
 
+string readFile(const std::string& fileName)
+{
+	ifstream file(fileName);
+	if (!file.bad())
+		return string();
+	istreambuf_iterator<char> begin(file);
+	istreambuf_iterator<char> end;
+	return string(begin, end);
+}
+
 void JsonParser::parse(const std::string& fileName)
 {
-	
+	json j = readFile(fileName);
+	GlobalVariable::instance().option()->setServerPort(j["server-port"]);
+	GlobalVariable::instance().option()->setServerHost(j["server-host"]);
+	GlobalVariable::instance().option()->setLocalPort(j["local-port"]);
+	GlobalVariable::instance().option()->setTimeout(j["timeout"]);
+	GlobalVariable::instance().option()->setPassword(j["password"]);
+	GlobalVariable::instance().option()->setMethod(j["encrypt-method"]);
 }
